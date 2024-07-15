70a156d0eb5304916d448a629649b1bb83267cc4
kantavit <krite-18354@hotmail.com>
2024-07-15 10:31:12 +0700

    fix:arrange function for readability

--- src/components/Table/TableMutation.tsx
+++ src/components/Table/TableMutation.tsx
@@ -2,17 +2,11 @@ import React from 'react'
 import TableCell from '@mui/material/TableCell';
 import { addRowFunction, deleteRowFunction, addColumnFunction, deleteColumnFunction } from '@/shared/mutation/tableData.mutation';
 
-export const DeleteColumn = (index: number, columns: string[], setModified: React.Dispatch<React.SetStateAction<boolean>>, setColumns: React.Dispatch<React.SetStateAction<string[]>>, setRowsData: React.Dispatch<React.SetStateAction<string[][]>>) => {
+export const AddRow = (columns: string[], setModified: React.Dispatch<React.SetStateAction<boolean>>, setRows: React.Dispatch<React.SetStateAction<number>>, setRowsData: React.Dispatch<React.SetStateAction<string[][]>>) => {
   return (
-    <TableCell key={index + 999999} align='center'>
-      <button
-        type="button"
-        className="bg-red-400 rounded-md p-1"
-        onClick={() => deleteColumnFunction(index, columns, setModified, setColumns, setRowsData)}
-      >
-        Delete
-      </button>
-  </TableCell>  
+    <button type="button" onClick={() => addRowFunction(columns, setModified, setRows, setRowsData)} className="bg-gray-300 rounded-md p-1">
+      Add Row
+    </button>
   )
 }
 
@@ -30,26 +24,25 @@ export const DeleteRow = (index: number, setModified: React.Dispatch<React.SetSt
   )
 }
 
-export const ColumnData = (index: number, columns: string[], readOnly: boolean, setColumns: React.Dispatch<React.SetStateAction<string[]>>, setModified: React.Dispatch<React.SetStateAction<boolean>>, setReadOnly: React.Dispatch<React.SetStateAction<boolean>>) => {
+export const AddColumn = (columns: string[], setModified: React.Dispatch<React.SetStateAction<boolean>>, setColumns: React.Dispatch<React.SetStateAction<string[]>>, setRowsData: React.Dispatch<React.SetStateAction<string[][]>>) => {
   return (
-    <TableCell key={index} scope="col" align='center'>
-      <input
-        type="text"
-        className="form-control border-0 text-center bg-dark text-black"
-        style={{ outline: "none", boxShadow: "none" }}
-        readOnly={readOnly}
-        onFocus={() => setReadOnly(false)}
-        onBlur={() => setReadOnly(true)}
-        value={columns[index]}
-        onChange={(e) =>
-          setColumns(
-            columns.map((coln, id) =>
-              id === index ? e.target.value : coln
-            )
-          )
-        }
-      />
-    </TableCell>
+    <button type="button" onClick={() => addColumnFunction(columns, setModified, setColumns, setRowsData)} className="bg-gray-300 rounded-md p-0.5" >
+      Add Column
+    </button>
+  )
+}
+
+export const DeleteColumn = (index: number, columns: string[], setModified: React.Dispatch<React.SetStateAction<boolean>>, setColumns: React.Dispatch<React.SetStateAction<string[]>>, setRowsData: React.Dispatch<React.SetStateAction<string[][]>>) => {
+  return (
+    <TableCell key={index + 999999} align='center'>
+      <button
+        type="button"
+        className="bg-red-400 rounded-md p-1"
+        onClick={() => deleteColumnFunction(index, columns, setModified, setColumns, setRowsData)}
+      >
+        Delete
+      </button>
+  </TableCell>  
   )
 }
 
@@ -79,18 +72,25 @@ export const RowData = (index: number, data: string[], rowsData: string[][], set
   )
 }
 
-export const AddRow = (columns: string[], setModified: React.Dispatch<React.SetStateAction<boolean>>, setRows: React.Dispatch<React.SetStateAction<number>>, setRowsData: React.Dispatch<React.SetStateAction<string[][]>>) => {
+export const ColumnData = (index: number, columns: string[], readOnly: boolean, setColumns: React.Dispatch<React.SetStateAction<string[]>>, setModified: React.Dispatch<React.SetStateAction<boolean>>, setReadOnly: React.Dispatch<React.SetStateAction<boolean>>) => {
+  const handleChange = (e: any) => {
+    setColumns(columns.map((coln, id) => id === index ? e.target.value : coln))
+  }
+  
   return (
-    <button type="button" onClick={() => addRowFunction(columns, setModified, setRows, setRowsData)} className="bg-gray-300 rounded-md p-1">
-      Add Row
-    </button>
-  )
-}
-
-export const AddColumn = (columns: string[], setModified: React.Dispatch<React.SetStateAction<boolean>>, setColumns: React.Dispatch<React.SetStateAction<string[]>>, setRowsData: React.Dispatch<React.SetStateAction<string[][]>>) => {
-  return (
-    <button type="button" onClick={() => addColumnFunction(columns, setModified, setColumns, setRowsData)} className="bg-gray-300 rounded-md p-0.5" >
-      Add Column
-    </button>
+    <TableCell key={index} scope="col" align='center'>
+      <input
+        type="text"
+        className="form-control border-0 text-center bg-dark text-black"
+        style={{ outline: "none", boxShadow: "none" }}
+        readOnly={readOnly}
+        onFocus={() => setReadOnly(false)}
+        onBlur={() => setReadOnly(true)}
+        value={columns[index]}
+        onChange={(e) =>
+          handleChange(e.target.value)
+        }
+      />
+    </TableCell>
   )
 }
